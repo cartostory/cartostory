e58fd382cd5c0999aa7df7178b24bd89106e5362
Michal Zimmermann <zimmicz@gmail.com>
2021-07-29 20:19:02 +0200

    Add proper types

---
+++ backend/global.d.ts
@@ -0,0 +1,4 @@
+declare module '@rearguard/is-email' {
+  // eslint-disable-next-line no-unused-vars
+  export function isEmail(maybeEmail: string): boolean;
+}
--- backend/src/routes/auth/sign-up.spec.ts
+++ backend/src/routes/auth/sign-up.spec.ts
@@ -1,7 +1,12 @@
 import { server } from '../../app';
 
 describe('sign-up', () => {
-  test('', () => {
+  test('', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/hello',
+    });
 
+    expect(response.statusCode).toEqual(200);
   });
 });
