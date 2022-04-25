610ea6c5c57017b5efe2242796668600e73c5041
Michal Zimmermann <zimmicz@gmail.com>
2022-04-25 14:38:48 +0200

    refactor(frontend): upload provider

--- packages/frontend/src/pages/stories/write/components/upload-button/index.tsx
+++ packages/frontend/src/pages/stories/write/components/upload-button/index.tsx
@@ -28,6 +28,34 @@ const positionClassnames = {
   topright: 'leaflet-top leaflet-right',
 }
 
+const UploadContext = React.createContext<
+  | (ReturnType<typeof useSelectFile> &
+      ReturnType<typeof useReadFile> &
+      ReturnType<typeof useLoadGeoJson>)
+  | undefined
+>(undefined)
+
+function useUploadContext() {
+  const context = React.useContext(UploadContext)
+
+  if (!context) {
+    throw new Error('UploadContext is only available inside UploadProvider.')
+  }
+}
+
+function UploadProvider({ children }: React.PropsWithChildren<unknown>) {
+  const [file, selectFile] = useSelectFile()
+  const content = useReadFile(file)
+  const loaded = useLoadGeoJson(
+    content?.status === 'done' ? content.result : undefined,
+  )
+  return (
+    <UploadContext.Provider value={{ file, selectFile, content, loaded }}>
+      {children}
+    </UploadContext.Provider>
+  )
+}
+
 function UploadButton({
   position = 'topleft',
 }: {
@@ -38,49 +66,26 @@ function UploadButton({
   const loaded = useLoadGeoJson(
     content?.status === 'done' ? content.result : undefined,
   )
-  const { uploadElementControls, uploadStatusControls } =
-    useUploadResultAnimation(file, loaded)
-
-  const uploadElement = (
-    <>
-      <input
-        onChange={selectFile}
-        type="file"
-        name="track"
-        accept=".json"
-        className="opacity-0 absolute w-[32px] h-[32px] cursor-pointer"
-      />
-      <motion.img
-        src={Upload}
-        className="basis-5"
-        initial={false}
-        animate={uploadElementControls}
-      />
-      {loaded ? (
-        <motion.img
-          animate={uploadStatusControls}
-          src={loaded === 'error' ? X : Check}
-          className="basis-5"
-        />
-      ) : null}
-    </>
-  )
 
   const children: {
     [key in FileReadingStatus['status'] | 'unknown']: JSX.Element
   } = {
-    done: uploadElement,
+    done: <UploadElement selectFile={selectFile} loaded={loaded} file={file} />,
     error: <>oh no</>,
     loading: <Loader />,
-    unknown: uploadElement,
+    unknown: (
+      <UploadElement selectFile={selectFile} loaded={loaded} file={file} />
+    ),
   }
 
   return (
-    <div className={positionClassnames[position]}>
-      <div className="leaflet-control leaflet-bar bg-white h-[32px] w-[32px] top-[73px] flex justify-center">
-        {children[content?.status ?? 'unknown']}
+    <UploadProvider>
+      <div className={positionClassnames[position]}>
+        <div className="leaflet-control leaflet-bar bg-white h-[32px] w-[32px] top-[73px] flex justify-center">
+          {children[content?.status ?? 'unknown']}
+        </div>
       </div>
-    </div>
+    </UploadProvider>
   )
 }
 
@@ -202,6 +207,7 @@ function useUploadResultAnimation(
     }
 
     async function start() {
+      console.log('start')
       uploadStatusControls.set({ scale: 0, display: 'none' })
       await uploadElementControls.start({
         scale: 0,
@@ -209,14 +215,16 @@ function useUploadResultAnimation(
     }
 
     async function switchElements() {
+      console.log('switchElements')
       uploadElementControls.set({ display: 'none' })
-      uploadStatusControls.set({ translateX: 0, display: 'initial' })
+      uploadStatusControls.set({ display: 'initial' })
       return uploadStatusControls.start({
         scale: 1,
       })
     }
 
     async function keepStatusVisible() {
+      console.log('keepStatusVisible')
       return new Promise(resolve => {
         setTimeout(() => {
           resolve(true)
@@ -225,12 +233,14 @@ function useUploadResultAnimation(
     }
 
     async function switchElementsBack() {
+      console.log('switchElementsBack')
       return uploadStatusControls.start({
         scale: 0,
       })
     }
 
     async function reset() {
+      console.log('reset')
       uploadStatusControls.set({ display: 'none' })
       uploadElementControls.set({ display: 'initial' })
       return uploadElementControls.start({ scale: 1 })
@@ -245,6 +255,45 @@ function useUploadResultAnimation(
   }, [uploadElementControls, uploadStatusControls, loaded, file])
 
   return { uploadStatusControls, uploadElementControls }
+}
+
+function UploadElement({
+  file,
+  loaded,
+  selectFile,
+}: {
+  file: ReturnType<typeof useSelectFile>['0']
+  loaded: ReturnType<typeof useLoadGeoJson>
+}) {
+  const { uploadElementControls, uploadStatusControls } =
+    useUploadResultAnimation(file, loaded)
+  return (
+    <>
+      <input
+        onChange={selectFile}
+        type="file"
+        name="track"
+        accept=".json"
+        className="opacity-0 absolute w-[32px] h-[32px] cursor-pointer"
+      />
+      <motion.img
+        src={Upload}
+        className="basis-5 block"
+        animate={uploadElementControls}
+        onAnimationStart={() => console.log('uploadElementControls started')}
+        onAnimationComplete={() => console.log('uploadElementControls ended')}
+      />
+      {loaded ? (
+        <motion.img
+          animate={uploadStatusControls}
+          src={loaded === 'error' ? X : Check}
+          className="basis-5 block"
+          onAnimationStart={() => console.log('uploadStatusControls started')}
+          onAnimationComplete={() => console.log('uploadStatusControls ended')}
+        />
+      ) : null}
+    </>
+  )
 }
 
 export { UploadButton }
