bd44338b96d28bb9fdab4e0e0f5072a8245fc823
Michal Zimmermann <zimmicz@gmail.com>
2022-06-03 15:36:45 +0200

    refactor(frontend): extract shadow component

--- packages/frontend/src/pages/stories/write/index.tsx
+++ packages/frontend/src/pages/stories/write/index.tsx
@@ -5,6 +5,7 @@ import { randomString } from '../../../utils'
 import { Editor } from './components/editor'
 import { Map } from './components/map'
 import { ActionProvider, StoryProvider } from './providers/story-provider'
+import { Map, Shadow, Story } from './components'
 
 function Write() {
   const [slug, setSlug] = useSlug()
@@ -15,11 +16,7 @@ function Write() {
         <div className="w-full flex">
           <div className="w-1/2 bg-gray-200">
             <Map />
-            <div
-              aria-hidden
-              className="absolute top-0 z-[10000] w-[10px] h-[100vh] right-1/2 pointer-events-none"
-              style={{ boxShadow: 'inset -5px 0px 3px 0px rgba(0, 0, 0, .15)' }}
-            />
+            <Shadow />
           </div>
           <div className="w-1/2 p-5 px-10 flex flex-col h-[100vh]">
             <form
